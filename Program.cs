5101664b284e2dda06238dd0f6c3d8b1fb3ec27f
Anna <anya_zhevnenko@mail.ru>
2023-06-12 21:46:54 +0300

    9

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// #Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// Console.WriteLine("Введите число N: ");
+// int number = int.Parse(Console.ReadLine());
+// void NumberCounter(int number)
+// {
+//     if (number < 0) Console.Write ($"{number} не натуральное число");
+//     if (number == 0) return;
+//     Console.Write("{0,4}", number);
+//     NumberCounter (number - 1);
+// }
+// NumberCounter (number);
+
+
+// #Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Console.WriteLine("Введите число M: ");
+// int numberM = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите число N: ");
+// int numberN = int.Parse(Console.ReadLine());
+// void GapNumberSum (int numberM, int numberN, int sum)
+// {
+//     if (numberM > numberN)
+//     {
+//         Console.WriteLine($"Сумма натуральных элементовв промежутке от M до N: {sum}");
+//         return;
+//     }
+//     sum = sum + (numberM++);
+//     GapNumberSum (numberM, numberN, sum);
+// }
+// GapNumberSum (numberM, numberN, 0);
+
+// #Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+//  Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+// Console.WriteLine("Введите значение m:");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите значение n:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int akkerman (int m, int n)
+//  {
+//     if (m==0) return n + 1;
+//     else if (n==0) return
+//     akkerman(m-1,1);
+//     else return akkerman(m-1, akkerman(m,n-1));
+//  }
+//  Console.WriteLine($"Функция Аккермана равно {akkerman(m,n)}");
